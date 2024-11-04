51955212ef8b5e2d85cbcb6044aa1eba80ed4d97
Nozadea Sasmina <97602896+nozadeasasmina@users.noreply.github.com>
2024-11-04 10:49:39 +0800

    Delete 14_study_case_1 directory

--- 14_study_case_1/main.py
+++
@@ -1,9 +0,0 @@
-from pacflix import PacflixUser
-
-user1 = PacflixUser('Elsa','Basic Plan',12,'rosy-123')
-#user1.check_benefit()
-#user1.check_plan()
-#user1.upgrade_plan('Premium Pla')
-user1.pick_plan('Premium Plan','rosy-123')
-
-
